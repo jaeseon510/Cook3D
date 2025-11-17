918f40c88e9232f415dfd7dfd68699954519be3b
jaeseon510 <jaeseon510@naver.com>
2025-11-18 03:50:47 +0900

    [faet] 상호작용 구현

--- Assets/01Script/GameInput.cs
+++ Assets/01Script/GameInput.cs
@@ -1,13 +1,26 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class GameInput : MonoBehaviour
 {
+
+    public event EventHandler OnInteractAction;
     private PlayerInputActions playerInputActions;
     private void Awake()
     {
         playerInputActions = new PlayerInputActions();
         playerInputActions.Player.Enable();
+        playerInputActions.Player.Interact.performed += Interact_performed;
+    }
+
+    private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        // if (OnInteractAction != null)
+        // {
+        //     OnInteractAction(this, EventArgs.Empty);
+        // }
+        OnInteractAction?.Invoke(this,EventArgs.Empty);
     }
     
     public Vector2 GetMovementVectorNomarlized()
@@ -16,7 +29,7 @@ public class GameInput : MonoBehaviour
 
         inputVector = inputVector.normalized;
 
-        Debug.Log(inputVector);
+        //Debug.Log(inputVector);
         return inputVector;
     }
 }
