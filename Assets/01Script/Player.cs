b85e12c5d9ad475da6445ba8e5be7b4929615c3e
jaeseon510 <jaeseon510@naver.com>
2025-11-17 22:52:05 +0900

    [refactor] 게임인풋시스템 구조 변경
    cinemachine추가 및 inputsystem 업데이트

--- Assets/01Script/Player.cs
+++ Assets/01Script/Player.cs
@@ -3,32 +3,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float moveSpeed = 7f;
+    [SerializeField]private float moveSpeed = 7f;
+    [SerializeField]private GameInput gameInput;
+    
+    
     private bool isWalking;
+    
+    
     private void Update()
     {
-
-        Vector2 inputVector = new Vector2(0, 0);
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y += 1;
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y -= 1;
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x -= 1;
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x += 1;
-        }
-        inputVector = inputVector.normalized;
+        Vector2 inputVector = gameInput.GetMovementVectorNomarlized();
         
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * Time.deltaTime * moveSpeed;
