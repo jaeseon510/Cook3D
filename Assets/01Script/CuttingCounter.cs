78e32efc86dfc5a2e4552fa669c56263aaaa2945
jaeseon510 <jaeseon510@naver.com>
2025-11-20 03:23:38 +0900

    [feat] 커팅 애니메이션 및 ui 추가

--- Assets/01Script/CuttingCounter.cs
+++ Assets/01Script/CuttingCounter.cs
@@ -1,9 +1,24 @@
+using System;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class CuttingCounter : BaseCounter
 {
+
+    public event EventHandler<OnProgressChangedEventArgs> OnProgressChanged;
+
+    public class OnProgressChangedEventArgs : EventArgs
+    {
+        public float progressNormalized;
+    }
+
+    public event EventHandler OnCut;
+    
     [SerializeField] private CuttingRecipeSO[] cuttingRecipeSOArray;
+
+    private int cuttingProgress;
+    
     public override void Interact(Player player)
     {
         if (!HasKitchenObject())
@@ -11,18 +26,28 @@ public class CuttingCounter : BaseCounter
             if (player.HasKitchenObject())
             {
                 // 이미 옮기는
-                player.GetKitchenObject().SetKitchenObjectParent(this);
+                if (HasRecipeWithInput(player.GetKitchenObject().GetKitchenObjectSO()))
+                {
+                    player.GetKitchenObject().SetKitchenObjectParent(this);
+                    cuttingProgress = 0;
+                    
+                    
+                    CuttingRecipeSO cuttingRecipeSO = getCuttingRecipeSOWithInput(GetKitchenObject().GetKitchenObjectSO());
+                    
+                    OnProgressChanged?.Invoke(this, new OnProgressChangedEventArgs { progressNormalized = (float)cuttingProgress/cuttingRecipeSO.cuttingProgressMax });
+                }
+
             }
             else
             {
-                
+
             }
         }
         else
         {
             if (player.HasKitchenObject())
             {
-                
+
             }
             else
             {
@@ -33,22 +58,52 @@ public class CuttingCounter : BaseCounter
 
     public override void InteractAlternate(Player player)
     {
-        if (HasKitchenObject())
+        if (HasKitchenObject() && HasRecipeWithInput(GetKitchenObject().GetKitchenObjectSO()))
         {
-            KitchenObjectSO outputkitchenObjectSO = GetOutputForInput(GetKitchenObject().GetKitchenObjectSO());
-            GetKitchenObject().DestorySelf();
+            //there is a kitchenobject here and it can be cut
+            cuttingProgress++;
+            OnCut?.Invoke(this, EventArgs.Empty);
+            CuttingRecipeSO cuttingRecipeSO = getCuttingRecipeSOWithInput(GetKitchenObject().GetKitchenObjectSO());
+            
+            OnProgressChanged?.Invoke(this, new OnProgressChangedEventArgs { progressNormalized = (float)cuttingProgress/cuttingRecipeSO.cuttingProgressMax });
 
-            KitchenObject.SpawnKitchenObject(outputkitchenObjectSO, this);
+            if (cuttingProgress >= cuttingRecipeSO.cuttingProgressMax)
+            {
+                KitchenObjectSO outputkitchenObjectSO = GetOutputForInput(GetKitchenObject().GetKitchenObjectSO());
+                GetKitchenObject().DestorySelf();
+
+                KitchenObject.SpawnKitchenObject(outputkitchenObjectSO, this);
+            }
         }
     }
 
+    private bool HasRecipeWithInput(KitchenObjectSO inputKitchenObjectSO)
+    {
+        
+        CuttingRecipeSO cuttingRecipeSO = getCuttingRecipeSOWithInput(inputKitchenObjectSO);
+        return cuttingRecipeSO != null;
+    }
+
     private KitchenObjectSO GetOutputForInput(KitchenObjectSO inputKitchenObjectSO)
+    {
+        CuttingRecipeSO cuttingRecipeSO = getCuttingRecipeSOWithInput(inputKitchenObjectSO);
+        if (cuttingRecipeSO != null)
+        {
+            return cuttingRecipeSO.output;
+        }
+        else
+        {
+            return null;
+        }
+    }
+    
+    private CuttingRecipeSO getCuttingRecipeSOWithInput(KitchenObjectSO inputKitchenObjectSO)
     {
         foreach (CuttingRecipeSO cuttingRecipeSo in cuttingRecipeSOArray)
         {
             if (cuttingRecipeSo.input == inputKitchenObjectSO)
             {
-                return cuttingRecipeSo.output;
+                return cuttingRecipeSo;
             }
         }
         return null;
---
+++ Assets/01Script/LookAtCamera.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    private void LateUpdate()
+    {
+        transform.LookAt(Camera.main.transform);
+    }
+    
+}
