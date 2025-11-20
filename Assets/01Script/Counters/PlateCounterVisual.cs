1b13732ffcd645b81edd23e420d4495256a3940d
jaeseon510 <jaeseon510@naver.com>
2025-11-21 04:15:17 +0900

    [feat] 접시 스폰

---
+++ Assets/01Script/Counters/PlateCounterVisual.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateCounterVisual : MonoBehaviour
+{
+    [SerializeField] private PlatesCounter plateCounter;
+    [SerializeField] private Transform counterTopPoint;
+    [SerializeField] private Transform plateVisualPrefab;
+    
+    private List<GameObject> plateVisualGameObjectList;
+    private void Awake()
+    {
+        plateVisualGameObjectList = new List<GameObject>();
+    }
+    
+    private void Start()
+    {
+        plateCounter.OnPlateSpawned += PlatesCounter_OnPlateSpawned;
+        plateCounter.OnPlateReMove += PlatesCounter_OnPlateRemoved;
+    }
+
+    private void PlatesCounter_OnPlateRemoved(object sender, EventArgs e)
+    {
+        GameObject plateGameObject = plateVisualGameObjectList[plateVisualGameObjectList.Count - 1];
+        plateVisualGameObjectList.Remove(plateGameObject);
+        Destroy(plateGameObject);
+    }
+
+    private void PlatesCounter_OnPlateSpawned(object sender, EventArgs e)
+    {
+        Transform plateVisualTransform = Instantiate(plateVisualPrefab, counterTopPoint);
+
+        float plateOffsetY = .1f;
+        plateVisualTransform.localPosition = new Vector3(0,plateOffsetY * plateVisualGameObjectList.Count, 0);
+        
+        plateVisualGameObjectList.Add(plateVisualTransform.gameObject);
+        
+    }
+    
+    
+}
